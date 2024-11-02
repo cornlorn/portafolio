2ea86631ddcff5d2d2d71a1fed784b70ad194924
Pablo Sosa <sosadavid4280@outlook.com>
2024-11-02 17:10:41 -0600

    Commit No. 1

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import purgecss from 'vite-plugin-purgecss';
+
+export default defineConfig({
+    build: {
+        outDir: 'dist',
+    },
+    css: {
+        preprocessorOptions: {
+            less: {},
+        },
+    },
+    plugins: [
+        purgecss({
+            content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+        })
+    ],
+});
